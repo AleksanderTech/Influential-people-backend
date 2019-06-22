d8db27e57ea4b7dff4bb2d9dbf5b7834326e8a6a
Aleksander <aleksanderroztrop@gmail.com>
2019-06-22 20:28:13 +0200

    refactoring

---
+++ src/main/java/com/alek/influentialpeople/controller/UserEndpoint.java
@@ -0,0 +1,11 @@
+package com.alek.influentialpeople.controller;
+
+public final class UserEndpoint {
+
+	private UserEndpoint() {
+	}
+
+	public static final String BASE = "/api/user";
+	public static final String ID = "api/user/{id}";
+
+}
