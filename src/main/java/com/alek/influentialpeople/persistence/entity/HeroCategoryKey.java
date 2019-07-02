d154c86a499116ef8597c524ff94d994b645af06
Aleksander <aleksanderroztrop@gmail.com>
2019-07-02 23:10:44 +0200

    serial versionUID

--- src/main/java/com/alek/influentialpeople/persistence/entity/HeroCategoryKey.java
+++ src/main/java/com/alek/influentialpeople/persistence/entity/HeroCategoryKey.java
@@ -8,6 +8,9 @@ import javax.persistence.Embeddable;
 @Embeddable
 public class HeroCategoryKey implements Serializable {
 
+	
+	private static final long serialVersionUID = 1434151397030649812L;
+	
 	@Column(name = "hero_id")
 	private int heroId;
 	@Column(name = "category_id")
