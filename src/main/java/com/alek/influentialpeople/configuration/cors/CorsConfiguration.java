f79f7eaba2964b772e6f19d3a1c806b136bf86b6
Aleksander <aleksander.roztropinski@gfieast.com>
2019-10-02 23:26:30 +0200

    Cors configured

---
+++ src/main/java/com/alek/influentialpeople/configuration/cors/CorsConfiguration.java
@@ -0,0 +1,18 @@
+package com.alek.influentialpeople.configuration.cors;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Value("${cors.origin}")
+    private String corsOrigin;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*").allowedOrigins(corsOrigin);
+    }
+}
