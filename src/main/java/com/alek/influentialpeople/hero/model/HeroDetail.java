24e7fd183287becf2e4d764955721cc37ef4ec21
Aleksander <aleksander.roztropinski@gfieast.com>
2019-11-07 17:11:50 +0100

    HeroDetail model added

---
+++ src/main/java/com/alek/influentialpeople/hero/model/HeroDetail.java
@@ -0,0 +1,11 @@
+package com.alek.influentialpeople.hero.model;
+
+import java.util.Set;
+
+public class HeroDetail {
+
+    private String fullName;
+    private String avatarImageUrl;
+    private Set<String> categories;
+    private int score;
+}
