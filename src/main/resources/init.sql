65d31851902b59654526280bfa01897a7ba7fc2f
Aleksander <aleksanderroztrop@gmail.com>
2020-02-29 23:02:30 +0100

    Cleanup added to setup db script

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -1,3 +1,7 @@
+DELETE FROM mysql.user WHERE user='influ';
+DROP DATABASE influential_people;
+DROP USER influ@localhost;
+
 CREATE DATABASE influential_people;
 
 CREATE USER 'influ'@'localhost' IDENTIFIED BY 'influ';
